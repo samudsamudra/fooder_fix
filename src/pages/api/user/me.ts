7a182400205eafc176c13b5c5904fb686a6602ea
samudsamudra <capelliosam@gmail.com>
2025-02-10 17:07:08 +0700

    fixing bug login

--- src/pages/api/user/me.ts
+++ src/pages/api/user/me.ts
@@ -6,6 +6,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
